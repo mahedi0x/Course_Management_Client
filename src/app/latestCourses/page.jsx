301c4e4c8ddaef2ee64c8b2c0d1db75994dbeb1f
Mahedi0x <mahedih.cse@gmail.com>
2025-11-28 11:08:56 +0600

    check latestCourse issues

---
+++ src/app/latestCourses/page.jsx
@@ -0,0 +1,20 @@
+import CourseCard from "@/components/CourseCard";
+import getLatestCourses from "@/lib/latestCourses";
+
+
+export default async function LatestCourses() {
+  const latestAllCourses = await getLatestCourses();
+
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto my-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 px-3">
+          {latestAllCourses.map((course) => (
+            <CourseCard key={course._id} course={course} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
--- src/components/latestCourses.jsx
+++ src/components/latestCourses.jsx
@@ -1,21 +1,21 @@
-import getLatestCourses from "@/lib/latestCourses";
-import React from "react";
-import CourseCard from "./CourseCard";
+// import getLatestCourses from "@/lib/latestCourses";
+// import React from "react";
+// import CourseCard from "./CourseCard";
 
-const LatestCourses = async () => {
-  const latestAllCourses = await getLatestCourses();
+// const LatestCourses = async () => {
+//   const latestAllCourses = await getLatestCourses();
 
-  return (
-    <div>
-      <div className="max-w-7xl mx-auto my-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 px-3">
-          {latestAllCourses.map((course) => (
-            <CourseCard key={course._id} course={course} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div className="max-w-7xl mx-auto my-20">
+//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 px-3">
+//           {latestAllCourses.map((course) => (
+//             <CourseCard key={course._id} course={course} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default LatestCourses;
+// export default LatestCourses;
